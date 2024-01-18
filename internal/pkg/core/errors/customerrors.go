4ebe9c06c90f6f87f5c4196552609ab88a6e9347
Sébastien Ferry <sferry@equativ.com>
2024-01-18 20:36:26 +0100

    add business logic layer

--- internal/pkg/core/errors/customerrors.go
+++ internal/pkg/core/errors/customerrors.go
@@ -1,11 +1,26 @@
 package customerrors
 
+import "fmt"
+
 type CustomError struct {
 	Message string
 }
 
 func (e CustomError) Error() string {
 	return e.Message
+}
+
+func (e ArgumentError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Message, e.Argument)
+}
+
+type ArgumentError struct {
+	Message  string
+	Argument string
+}
+
+func NewArgumentError(argument string) ArgumentError {
+	return ArgumentError{Message: "Argument error", Argument: argument}
 }
 
 var ErrExisting error = CustomError{Message: "Already existing"}
